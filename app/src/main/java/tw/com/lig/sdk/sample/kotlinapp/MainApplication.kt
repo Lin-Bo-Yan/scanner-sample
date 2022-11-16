0ddff5cb28abd22aaa01fc7b03cecbd278f2fe96
Eliot Chu <eliot.chu@lig.com.tw>
2022-11-16 19:28:24 +0800

    Upgrade the scanner library to the 3.1.0 version.
    - Supported the product key.

--- app/src/main/java/tw/com/lig/sdk/sample/kotlinapp/MainApplication.kt
+++ app/src/main/java/tw/com/lig/sdk/sample/kotlinapp/MainApplication.kt
@@ -5,9 +5,13 @@ import tw.com.lig.sdk.scanner.LiGScanner
 
 class MainApplication: Application() {
 
+    companion object {
+        private const val SDK_PRODUCT_KEY = "B62D1-AED87-2C3B0-54A1F-D09D2"
+    }
+
     override fun onCreate() {
         super.onCreate()
-        LiGScanner.initialize(this)
+        LiGScanner.initialize(this, SDK_PRODUCT_KEY)
     }
 
     override fun onTerminate() {
